7548b9903996c18a3682e7007b6d10d699a9b4b9
fly2 <274646915@qq.com>
2017-02-17 20:42:18 +0800

    用opencv做图片处理

--- drop_dup.py
+++ drop_dup.py
@@ -4,16 +4,19 @@ Created on Thu Feb 16 13:57:13 2017
 
 @author: shangguanxf
 """
-from PIL import Image
+import cv2
 import os
+import numpy as np
 #去点重复的图片，path为去重文件所在路径,路径下只能有图片格式
 #path需要/左斜线，windows路径复制是右斜线
 def drop_duplicated(path):
     res={}
-    size=8,8
     for root,dirs,files in os.walk(path):
         for file in files:
         #去除非图片类型的文件
         #if not file.endswith('.txt'):
-             im = Image.open(os.path.join(root,file))
-             im.thumbnail(size, Image.ANTIALIAS)
+            p=os.path.join(root,file)
+            im = cv2.imread(p,0)
+            im=cv2.resize(im,(8,8))
+            
+             
